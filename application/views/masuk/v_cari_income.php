eda8c92cff56e36d1eb4adea0bf869fd7c8c8f8c
Archedar <ogickarecka1108@gmail.com>
2022-12-07 21:30:26 +0700

    Rework and Remake the dashboard, all stock, incoming design

--- application/views/masuk/v_cari_income.php
+++ application/views/masuk/v_cari_income.php
@@ -1,76 +1,48 @@
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-        	<div class="col-md-12">
-        		<h3 class="page-header" style="margin-top: 15px;margin:10px;">Incoming<small></small></h3>
-        		<div class="col-sm-8">
-            <div>
-          		<a href="<?php echo base_url('masuk/form_input'); ?>" data-toggle="tooltip" title="Add"><i class="fa fa-plus-circle fa-2x" style="margin-bottom: 10px;padding: 0 0 0 5px;color:#5aab26;"></i></a>
-          		<a href="<?php echo base_url('masuk/filter_cetak_masuk') . '/' . $temp_dari . '/' . $temp_sampai; ?>" data-toggle="tooltip" title="Print"><i class="fa fa-print fa-2x" style="margin-bottom: 10px;padding: 0 0 0 5px;color:#5aab26;"></i></a>
-          		<a href="<?php echo base_url('masuk/filter_excel_masuk') . '/' . $temp_dari . '/' . $temp_sampai; ?>" data-toggle="tooltip" title="Excel"><i class="fa fa-file-excel-o fa-2x" style="margin-bottom: 10px;padding: 0 0 0 5px;color:#5aab26;"></i></a>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/resources/style/incoming.css">
+        <div class="container-incoming">
+            <h3>Barang Masuk</h3>
 
-              </div>
-                <!--form class="form-inline">
-                  <div class="form-group">
-                    <input type="text" class="form-control form-control-sm" id="dari" placeholder="Dari tgl" style="height: 25px;font-size:12px">
-                  </div>
-                  <div class="form-group">
-                    <input type="text" class="form-control" id="dar" placeholder="Sampai tgl" style="height: 25px;font-size:12px;">
-                  </div>
-                  <button type="submit" name="simpan" class="btn btn-primary" style="height: 25px;font-size: 10px;padding: 3px 10px;">Cari</button>
-                </form-->
-
-              <div>
-                
-                  <h6>Periode: <?php echo "$temp_dari"; ?> s/d <?php echo "$temp_sampai"; ?></h6>
-          	        <table id="example" class="table table-bordered" width="100%">
-                        <thead>
-                            <tr style="font-size: 14px;">
-                                <th style='text-align: center;padding:6px;width: 40px;'>No</th>
-                                <th style='text-align: center;padding:6px;'>Nama Barang</th>
-                                <th style='text-align: center;padding:6px;width: 100px;'>Kd Barang</th>
-                                <th style='text-align: center;padding:6px;width: 100px;'>Size</th>
-                                <th style='text-align: center;padding:6px;width:60px;'>Qty</th>
-                                <th style='text-align: center;padding:6px;width:60px;'>Uom</th>
-                                <th style='text-align: center;padding:6px;width: 140px;'>Tgl Masuk</th>
-                                <th style='text-align: center;padding:6px;width:60px;'>Loc</th>
-                                <th style='text-align: center;padding:6px;width:100px;'>Suplier</th>
-                            </tr>
-                        </thead>
-                        
-                        <tbody>
-                          <?php
-                            $no = 1;
-                            foreach($stock as $row) {                  
-                              echo "<tr style='font-size: 12px;'>";
-                              echo "<td style='text-align: center;padding: 4px;'>".$no."</td>";
-                              echo "<td style='text-align: center;padding: 4px;'>".$row->nama_barang."</td>";
-                              echo "<td style='text-align: center;padding: 4px;'>".$row->kode_barang."</td>";
-                              echo "<td style='text-align: center;padding: 4px;'>".$row->kategori."</td>";                 
-                              echo "<td style='text-align: center;padding: 4px;'>".$row->jumlah."</td>";
-                              echo "<td style='text-align: center;padding: 4px;'>".$row->satuan."</td>";
-                              echo "<td style='text-align: center;padding: 4px;'>".$row->tgl."</td>";
-                              echo "<td style='text-align: center;padding: 4px;'>".$row->location."</td>";
-                              echo "<td style='text-align: center;padding: 4px;'>".$row->suplier."</td>";
-                              $no++;
-                              }
-                            ?>
-                            </tr>
-                          </tbody>
-                        </table>
-              </div>
-				</div>
-			</div>
-		</div>
-		<div class="clearfix"> </div>
-	<div class="drop-menu">
-		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu4">
-		  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Regular link</a></li>
-		  <li role="presentation" class="disabled"><a role="menuitem" tabindex="-1" href="#">Disabled link</a></li>
-		  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another link</a></li>
-		</ul>
-	</div>
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-  </body>
+            <p>Periode: <?php echo "$temp_dari"; ?> s/d <?php echo "$temp_sampai"; ?></p>
+            <br>
+            <table>
+                <thead>
+                    <tr>
+                        <th style='text-align: center;padding:6px;width: 40px;'>No</th>
+                        <th style='text-align: center;padding:6px;'>Nama Barang</th>
+                        <th style='text-align: center;padding:6px;width: 160px;'>Kd Barang</th>
+                        <th style='text-align: center;padding:6px;width: 140px;'>Kategori</th>
+                        <th style='text-align: center;padding:6px;width:60px;'>Qty</th>
+                        <th style='text-align: center;padding:6px;width:60px;'>Uom</th>
+                        <th style='text-align: center;padding:6px;width: 160px;'>Tanggal Masuk</th> 
+                        <th style='text-align: center;padding:6px;width:60px;'>Loc</th>
+                        <th style='text-align: center;padding:6px;width:90px;'>Suplier</th>
+                    </tr>
+                </thead>
+                                  
+                <tbody>
+                    <?php $no = 1; foreach($stock as $row) { ?>
+                        <tr class='unik'>
+                            <td style='text-align: center;padding: 4px;'><?= $no; ?></td>
+                            <td style='padding: 4px;'><?= $row->nama_barang; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->kode_barang; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->kategori; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->jumlah; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->satuan; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->tgl; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->location; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->suplier; ?></td>
+                          </tr>
+                    <?php $no++; } ?>
+                </tbody>
+            </table><br>
+            <li>
+                <a href="<?php echo base_url('masuk/form_input'); ?>"><iconify-icon icon="ic:baseline-plus" class="icon"></iconify-icon><span>Add Incoming Item</span></a>
+            </li>
+            <div class="list-cetak">
+                <a href="<?php echo base_url('masuk/filter_cetak_masuk') . '/' . $temp_dari . '/' . $temp_sampai; ?>">Cetak PDF</a>
+                <a href="<?php echo base_url('masuk/filter_excel_masuk') . '/' . $temp_dari . '/' . $temp_sampai; ?>">Cetak XLS</a>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
--- application/views/masuk/v_masuk.php
+++ application/views/masuk/v_masuk.php
@@ -1,106 +1,52 @@
-<style type="text/css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/resources/style/incoming.css">
+        <div class="container-incoming">
+            <h3>Barang Masuk</h3>
 
-.dataTables_filter{visibility: hidden;}
-.dataTables_length{visibility: hidden;}
-.tgl{float: right;}
-.submite{float: right;}
-.pagination{float: left;}
-.dataTables_info{visibility: hidden;}
-.pagination {float: right;}
-</style>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-        	<div class="col-md-12">
-        		<h3 class="page-header" style="margin-top: 15px;margin:10px;">Incoming<small></small></h3>
-        		<div class="col-sm-8">
-        		<a href="<?php echo base_url('masuk/form_input'); ?>" data-toggle="tooltip" title="Add"><i class="fa fa-plus-circle fa-2x" style="margin-bottom: 10px;padding: 0 0 0 5px;color:#5aab26;"></i></a>
-        		<a href="<?php echo base_url('masuk/cetak_masuk'); ?>" data-toggle="tooltip" title="Print"><i class="fa fa-print fa-2x" style="margin-bottom: 10px;padding: 0 0 0 5px;color:#5aab26;"></i></a>
-            <a href="<?php echo base_url('masuk/excel_masuk'); ?>" data-toggle="tooltip" title="Excel"><i class="fa fa-file-excel-o fa-2x" style="margin-bottom: 10px;padding: 0 0 0 5px;color:#5aab26;"></i></a>
-
-                <form class="form-inline" method="post" action="<?php echo base_url('masuk/hasil_cari_masuk'); ?>" style="margin-bottom: -6px;">
-                  <div class="form-group">
-                    <input type="text" name="dari" class="form-control form-control-sm" required  id="dari" placeholder="Dari tgl" style="height: 25px;font-size:12px">
-                  </div>
-                  <div class="form-group">
-                    <input type="text" name="sampai" class="form-control" required  id="dar" placeholder="Sampai tgl" style="height: 25px;font-size:12px;">
-                  </div>
-                  <input type="submit" value="cari" class="btn btn-primary" style="height: 25px;font-size: 10px;padding: 3px 10px;">
-                </form>
-
-        	        <table id="example" class="table table-bordered data" width="100%">
-                      <thead>
-                          <tr style="font-size: 14px;">
-                              <th style='text-align: center;padding:6px;width: 40px;'>No</th>
-                              <th style='text-align: center;padding:6px;'>Nama Barang</th>
-                              <th style='text-align: center;padding:6px;width: 100px;'>Kd Barang</th>
-                              <th style='text-align: center;padding:6px;width: 100px;'>Kategori</th>
-                              <th style='text-align: center;padding:6px;width:60px;'>Qty</th>
-                              <th style='text-align: center;padding:6px;width:60px;'>Uom</th>
-                              <th style='text-align: center;padding:6px;width: 140px;'>Tgl Masuk</th> 
-                              <th style='text-align: center;padding:6px;width:60px;'>Loc</th>
-                              <th style='text-align: center;padding:6px;width:60px;'>Suplier</th>
+            <form action="<?php echo base_url('masuk/hasil_cari_masuk'); ?>" method="POST">
+                <input type="date" name="dari">
+                <input type="date" name="sampai">
+                <input type="submit" value="Cari">
+            </form>
+            <br>
+            <table>
+                <thead>
+                    <tr>
+                        <th style='text-align: center;padding:6px;width: 40px;'>No</th>
+                        <th style='text-align: center;padding:6px;'>Nama Barang</th>
+                        <th style='text-align: center;padding:6px;width: 160px;'>Kd Barang</th>
+                        <th style='text-align: center;padding:6px;width: 140px;'>Kategori</th>
+                        <th style='text-align: center;padding:6px;width:60px;'>Qty</th>
+                        <th style='text-align: center;padding:6px;width:60px;'>Uom</th>
+                        <th style='text-align: center;padding:6px;width: 160px;'>Tanggal Masuk</th> 
+                        <th style='text-align: center;padding:6px;width:60px;'>Loc</th>
+                        <th style='text-align: center;padding:6px;width:90px;'>Suplier</th>
+                    </tr>
+                </thead>
+                                  
+                <tbody>
+                    <?php $no = 1; foreach($masuk as $row) { ?>
+                        <tr class='unik'>
+                            <td style='text-align: center;padding: 4px;'><?= $no; ?></td>
+                            <td style='padding: 4px;'><?= $row->nama_barang; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->kode_barang; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->kategori; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->jumlah; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->satuan; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->tgl; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->location; ?></td>
+                            <td style='text-align: center;padding: 4px;'><?= $row->suplier; ?></td>
                           </tr>
-                      </thead>
-                      
-                      <tbody>
-
-                            <?php $no = 1; foreach($masuk as $row) { ?>
-                            <tr style='font-size: 12px;'>
-							    <td style='text-align: center;padding: 4px;'><?= $no; ?></td>
-							    <td style='text-align: center;padding: 4px;'><?= $row->nama_barang; ?></td>
-                                <td style='text-align: center;padding: 4px;'><?= $row->kode_barang; ?></td>
-                                <td style='text-align: center;padding: 4px;'><?= $row->kategori; ?></td>                
-                                <td style='text-align: center;padding: 4px;'><?= $row->jumlah; ?></td>
-                                <td style='text-align: center;padding: 4px;'><?= $row->satuan; ?></td>
-                                <td style='text-align: center;padding: 4px;'><?= $row->tgl; ?></td>
-                                <td style='text-align: center;padding: 4px;'><?= $row->location; ?></td>
-                                <td style='text-align: center;padding: 4px;'><?= $row->suplier; ?></td>
-							</tr>
-							<?php $no++; } ?>
-                      </tbody>
-                    </table>
-				</div>
-			</div>
-		</div>
-		<div class="clearfix"> </div>
-	<div class="drop-menu">
-		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu4">
-		  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Regular link</a></li>
-		  <li role="presentation" class="disabled"><a role="menuitem" tabindex="-1" href="#">Disabled link</a></li>
-		  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another link</a></li>
-		</ul>
-	</div>
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo base_url(); ?>/resources/js/bootstrap.min.js"></script>
-        <script src="<?php echo base_url(); ?>/resources/js/bootstrap.min.js"></script>
-        <script src="<?php echo base_url(); ?>/resources/js/jquery-3.1.1.min.js"></script>
-        <script src="<?php echo base_url(); ?>/resources/js/bootstrap.js"></script>
-        <script src="<?php echo base_url(); ?>/resources/js/bootstrap-datepicker.js"></script>
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $('#dari').datepicker({
-                    format: "yyyy-mm-dd",
-                    autoclose:true
-                });
-            });
-        </script>
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $('#dar').datepicker({
-                    format: "yyyy-mm-dd",
-                    autoclose:true
-                });
-            });
-        </script>
-        <script src="<?php echo base_url(); ?>/resources/js/bootstrap.min.js"></script>
-        <script src="<?php echo base_url(); ?>/resources/js/jquery.dataTables.min.js"></script>
-        <script src="<?php echo base_url(); ?>/resources/js/dataTables.bootstrap.js"></script>
-        <script type="text/javascript">
-            $(function() {
-                $('#example').dataTable();
-            });
-        </script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-  </body>
+                    <?php $no++; } ?>
+                </tbody>
+            </table><br>
+            <li>
+                <a href="<?php echo base_url('masuk/form_input'); ?>"><iconify-icon icon="ic:baseline-plus" class="icon"></iconify-icon><span>Add Incoming Item</span></a>
+            </li>
+            <div class="list-cetak">
+                <a href="<?php echo base_url('masuk/cetak_masuk'); ?>">Cetak PDF</a>
+                <a href="<?php echo base_url('masuk/excel_masuk'); ?>">Cetak XLS</a>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
